978b09bda2a5688a7c2c1add89f17df9fbd756e6
Erico Andrei <erico@simplesconsultoria.com.br>
2009-07-09 19:29:08 +0000

    Implements localization to the product

--- sc/pfg/brazilianfields/__init__.py
+++ sc/pfg/brazilianfields/__init__.py
@@ -5,9 +5,11 @@ from Products.Archetypes.public import process_types, listTypes
 from Products.CMFCore import utils
 from Products.CMFCore.DirectoryView import registerDirectory
 
+from zope.i18nmessageid import MessageFactory as BaseMessageFactory
+MessageFactory = BaseMessageFactory('sc.pfg.brazilianfields')
+
 from config import *
 import fields
-
 
 def initialize(context):
     content_types, constructors, ftis = process_types(
